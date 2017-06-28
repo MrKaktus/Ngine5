477ed43fa041196a6e7571fe030295c7a7472878
Karol Gasinski <kgasinski@apple.com>
2017-06-27 19:31:23 -0700

    Started adding support for Metal Descriptor Tables.

--- src/core/rendering/metal/mtlLayout.mm
+++ src/core/rendering/metal/mtlLayout.mm
@@ -24,10 +24,80 @@ namespace en
 {
    namespace gpu
    {
+   static const MTLDataType TranslateDataType[underlyingType(ResourceType::Count)] =
+      {
+      MTLDataTypeSampler,   // Sampler
+      MTLDataTypeTexture,   // Texture
+      MTLDataTypeTexture,   // Image
+      MTLDataTypePointer,   // Uniform
+      MTLDataTypePointer,   // Storage
+      };
+   
+   static const MTLArgumentType TranslateArgumentType[underlyingType(ResourceType::Count)] =
+      {
+      MTLArgumentTypeSampler,   // Sampler
+      MTLArgumentTypeTexture,   // Texture
+      MTLArgumentTypeTexture,   // Image
+      MTLArgumentTypeBuffer ,   // Uniform
+      MTLArgumentTypeBuffer ,   // Storage
+      };
+
+    
+      
+   // stageMask is ignored in Metal
    Ptr<SetLayout> MetalDevice::createSetLayout(const uint32 count,
                                                const ResourceGroup* group,
                                                const ShaderStages stageMask)
    {
+   assert( count );
+   assert( group );
+   
+   Ptr<SetLayoutMTL> result = nullptr;
+
+   uint32 types = underlyingType(ResourceType::Count);
+   uint32* slot = new uint32[types];
+   for(uint32 i=0; i<types; ++i)
+      slot[i] = 0u;
+      
+   NSMutableArray* rangeInfo = [NSMutableArray arrayWithCapacity:count];
+   
+   //MTLIndirectArgumentDescriptor* rangeInfo = new MTLIndirectArgumentDescriptor[count];
+   for(uint32 i=0; i<count; ++i)
+      {
+      rangeInfo[i] = [MTLIndirectArgumentDescriptor alloc];
+      
+      // Single Descriptors range
+      uint32 resourceType = underlyingType(group[i].type);
+     
+      rangeInfo[i].argumentType = TranslateArgumentType[resourceType];
+    MTLDataType       dataType;
+      rangeInfo[i].index       = slot[resourceType];
+      rangeInfo[i].arrayLength = group[i].count;
+      rangeInfo[i].access      = MTLArgumentAccessReadWrite;
+      rangeInfo[i].textureType =
+      
+      rangeInfo[i].index       = slot[resourceType];
+      rangeInfo[i].dataType    = TranslateResourceType[resourceType];
+      rangeInfo[i].arrayLength = group[i].count;
+      rangeInfo[i].access      = MTLArgumentAccessReadWrite; // Resource access type (read/write/readWrite)
+                              // MTLArgumentAccessReadOnly
+                              // MTLArgumentAccessReadWrite
+                              // MTLArgumentAccessWriteOnly
+      rangeInfo[i].textureType =
+
+      // Increase index by amount of slots used
+      slot[resourceType] += group[i].count;
+      }
+
+
+   NSArray<MTLIndirectArgumentDescriptor *>* rangeInfo =
+   
+   //NSArray<MTLIndirectArgumentDescriptor *> *)arguments
+
+   MTLIndirectArgumentDescriptor* descriptor = [MTLIndirectArgumentDescriptor indirectArgumentDescriptor];
+   
+
+   
    // TODO: Finish / Emulate !
    return Ptr<SetLayout>(nullptr);
    }
@@ -38,6 +108,11 @@ namespace en
                                                          const Ptr<Sampler>* sampler,
                                                          const ShaderStages stageMask)
    {
+
+   id<MTLIndirectArgumentEncoder> layout = nil;
+
+   // - (id<MTLIndirectArgumentEncoder>)newIndirectArgumentEncoderWithArguments:(NSArray<MTLIndirectArgumentDescriptor *> *)arguments;
+   
    // TODO: Finish / Emulate !
    return Ptr<PipelineLayout>(nullptr);
    }
