fdae117f5b66df1b84ce731ee819f809c521ef6d
MrKaktus <kuktus@gmail.com>
2026-05-11 22:45:20 -0700

    Cleanup of Parser class, and separation of common state, now shared with JSON parser.

--- public/include/core/utilities/parser.h
+++ public/include/core/utilities/parser.h
@@ -18,16 +18,100 @@
 namespace en
 {
 
-class Parser
+enum class ParsingResult : uint8
 {
-    private:
-    uint8* buffer; // Pointer to text that need to be parsed
-    uint64 offset; // Current offset in text
-    uint64 size;   // Text size
-    
-    public:
-    Parser(uint8* buffer, uint64 size); 
+    Success        = 0,
+    IncompleteData = 1, // Provided buffer was not big enough to finish parsing
+    InvalidFormat  = 2, // Buffer contents are not following expected parsing rules
+    Unsupported    = 3, // Result of parsing points at unsupported configuration
+};
+
+enum ParserType
+{
+    None     = 0,
+    String      ,
+    Integer     ,
+    Float       ,
+    EndOfLine   ,
+  //EndOfFile   ,          
+};
+
+class ParserState
+{
+protected:
+    const uint8* buffer; // Pointer to text that need to be parsed
+    uint64 offset;       // Current offset in text
+    const uint64 size;   // Text size
+
+    // When String element is detected, it's length is calculated,
+    // and offset is moved to first character after that string.
+    // This allows easy operations like comparison on detected string.
+    uint64 foundStringOffset;
+    sint32 foundStringLength;
+
+    // When Number is detected, its parsed in advance, calculating 
+    // it's length in characters, and detecting it's type. Offset is 
+    // moved to first character after that number. Then based on 
+    // it's type, final value can be extracted (if needed).
+    uint64 foundNumberOffset;
+    uint64 foundNumberLength;
+
+public:
+    // Passes ownership of buffer to parser
+    ParserState(const uint8* buffer, const uint64 size);
+   ~ParserState();
+};
+
+class Parser : public ParserState
+{
+protected:
+    ParserType type;     // Last found type
+
+    // If characters sequence at specified offset location 
+    // is representing float value, returns length of that
+    // string representation.
+    bool isFloat(const uint64 startOffset, uint64& length);
+
+    // If characters sequence at specified offset location 
+    // is representing integer value, returns length of that
+    // string representation. This check should be done
+    // after isFloat one.
+    bool isInteger(const uint64 startOffset, uint64& length);
+
+    // Returns true if there is characters sequence at
+    // specified offset location.
+    bool isString(const uint64 startOffset, sint32& length);
+
+public:
+    // Passes ownership of buffer to parser
+    Parser(const uint8* buffer, const uint64 size); 
    ~Parser();
+
+    // Progresses in parsed buffer to next parsable element
+    // (it does not follow any specific format rules, as 
+    // opposed to well defined formats like JSON).
+    virtual ParserType findNextElement(void);
+
+    // Returns type of currently detected element
+    ParserType currentElement(void) const;
+
+    // If isInteger() returned true, below methods convert
+    // string representation into integer value. False is
+    // returned if value is negative but U64 is expected.
+    // False is also returned if parsed value doesn't fit
+    // into S64 range.
+    bool readU64(uint64& value);
+    bool readS64(sint64& value);
+
+    bool readF32(float& value);
+    bool readF64(double& value);
+
+    bool isStringMatching(const char* string);
+
+    const char* string(void);
+    uint32 stringLength(void);
+
+
 
     bool position(uint64 offset);            // Sets current position in buffer
     bool read(std::string& word, bool& eol); // Reads single word, character sequence ended with whitespace. Sets EOL to true if reached EOL.
@@ -36,14 +120,55 @@ class Parser
     bool end(void);                          // Returns true if offset reached end of buffer
 };
 
-bool isCypher(uint8 input);
-bool isCharacter(uint8 input);
-bool isWhitespace(uint8 input);
-bool isEol(uint8 input);
+// [0..9]
+bool isCypher(const uint8 input);
+
+// [0..9][A..F][a..f]
+bool isHexCypher(const uint8 input);
+
+// [A..Z]
+bool isUpperCaseLetter(const uint8 input);
+
+// [a..z]
+bool isLowerCaseLetter(const uint8 input);
+
+// [A..Z][a..z]
+bool isLetter(const uint8 input);
+
+// All printable characters [!..~]
+bool isCharacter(const uint8 input);
+
+// Space & horizontal tabulator
+bool isWhitespace(const uint8 input);
+
+// Carriage return (CR), line feed (LF), vertical tab, form feed
+bool isEol(const uint8 input);
+
+// TODO: Below should all return ParsingResult if they parse provided buffer
+
 // Length is expected length of provided integer string representation
 bool isInteger(const char* text, const uint32 length);
 // Length is expected length of provided float string representation
 bool isFloat(const char* text, const uint32 length);
+
+// Returns length of detected string with terminating zero.
+// For empty string will return Success and length of 1.
+ParsingResult parseString(const uint8* buffer, const uint64 size, uint64& length);
+
+// Returns success if buffer content is byte sequence ending with EOL (LF or CRLF).
+// In such case returned length is length of line without EOL bytes, while offset
+// points to what would be next byte after EOL signature (so line length plus size 
+// of EOL bytes).
+ParsingResult isLine(const uint8* buffer, const uint64 size, uint64& length, uint64& offset);
+
+// Returns success if buffer content is string followed by EOL (LF or CRLF) 
+// and its matching input string (which is null terminated but that null is 
+// not taken into notice during comparison). In such case returned length is 
+// offset to what would be next byte after EOL signature (so string length 
+// plus size of EOL bytes).
+ParsingResult isLineMatching(const uint8* buffer, const uint64 size, const char* string, uint64& length);
+
+bool nextWord(const std::string_view& line, uint64& lineOffset, std::string_view& word);
 
 } // en
 
--- src/core/utilities/parser.cpp
+++ src/core/utilities/parser.cpp
@@ -18,20 +18,35 @@
 namespace en
 {
 
-Parser::Parser(uint8* _buffer, uint64 _size) :
+ParserState::ParserState(const uint8* _buffer, const uint64 _size) :
     buffer(_buffer),
     offset(0),
-    size(_size)
+    size(_size),
+    foundStringOffset(0),
+    foundStringLength(-1),
+    foundNumberOffset(0),
+    foundNumberLength(-1)
 {
     assert(buffer);
     assert(size);
 }
 
-Parser::~Parser()
+ParserState::~ParserState()
 {
     delete [] buffer;
 }
 
+Parser::Parser(const uint8* _buffer, const uint64 _size) :
+    ParserState(_buffer, _size),
+    type(ParserType::None)
+{
+}
+
+Parser::~Parser()
+{
+}
+
+// TODO: Deprecate
 bool Parser::position(uint64 newOffset)
 {
     // Check if offset is in range
@@ -43,6 +58,435 @@ bool Parser::position(uint64 newOffset)
     offset = newOffset;
     return true;
 }
+
+ParserType Parser::findNextElement(void)
+{
+    uint8 byte = 0;
+    while(offset < size)
+    {
+        byte = buffer[offset++];
+
+        // Skips whitespaces until parseable element is found
+        if (isWhitespace(byte))
+        {
+            continue;
+        }
+
+        if (isCharacter(byte))
+        {
+            // Restores offset to point at first character of string representation
+            offset--;
+
+            if (isFloat(offset, foundNumberLength))
+            {                
+                foundNumberOffset = offset;
+                offset += foundNumberLength;
+                type = ParserType::Float;
+                return type;
+            }
+            else
+            if (isInteger(offset, foundNumberLength))
+            {
+                foundNumberOffset = offset;
+                offset += foundNumberLength;
+                type = ParserType::Integer;
+                return type;
+            }
+            else // Some kind of string
+            {
+                if (!isString(offset, foundStringLength))
+                {
+                    assert( 0 );
+                }
+
+                foundStringOffset = offset;
+                type = ParserType::String;
+                offset += foundStringLength;
+                return type;
+            }
+        }
+        else
+        if (isEol(byte))
+        {
+            // EOL is not implicitly skipped, but explicitly reported back
+            // to allow application parsing logic taking it into notice.
+            type = ParserType::EndOfLine;
+            return type;
+        }
+
+        // How did we got here?
+        assert(0);
+    }
+
+    return ParserType::None;
+}
+
+ParserType Parser::currentElement(void) const
+{
+    return type;
+}
+
+bool Parser::isFloat(const uint64 startOffset, uint64& length)
+{
+    // Floating point notation:
+    // 
+    // [-/+][cccc].cccc[e/E][-][cccc][f/F]
+    //
+    // - negative sign is optional
+    // - integer part or fractional part is optional
+    // - dot is required
+    // - e notation is optional
+    // - f/d postfix is optional
+    // - value may be immediately followed by other string (like arithmetic opoeration sign, etc.)
+    //
+    // Minimum case: .x
+    if (startOffset + 2 > size)
+    {
+        return false;
+    }
+
+    uint64 currentOffset = startOffset;
+
+    // Verify optional sign
+    bool sign = false;
+    if (buffer[currentOffset] == '-' ||
+        buffer[currentOffset] == '+')
+    {
+        sign = true;
+        ++currentOffset;
+    }
+
+    // Verify optional integer part
+    bool integerPart = false;
+    if (isCypher(buffer[currentOffset]))
+    {
+        integerPart = true;
+        for(++currentOffset; currentOffset<size; ++currentOffset)
+        {
+            if (isCypher(buffer[currentOffset]))
+            {
+                continue;
+            }
+
+            if (buffer[currentOffset] == '.')
+            {
+                break;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // Case parsed:
+        // [-/+]cccc[EOF]
+        if (currentOffset == size)
+        {
+            return false;
+        }
+    }
+
+    // At this point possible cases were parsed:
+    // [-/+][cccc.]
+    // empty
+
+    // Parser should point now at required dot
+    if (buffer[currentOffset] != '.')
+    {
+        return false;
+    }
+
+    // Skip required dot
+    ++currentOffset;
+
+    if (currentOffset == size)
+    {
+        // '-.' / '+.' strings are not a number
+        if (sign && !integerPart)
+        {
+            return false;
+        }
+    }
+
+    // If there is no integer part, fractional part is required
+    if (!integerPart &&
+        !isCypher(buffer[currentOffset]))
+    {
+        return false;
+    }
+
+    // Skip fractional part
+    for (; currentOffset<size; ++currentOffset)
+    {
+        if (!isCypher(buffer[currentOffset]))
+        {
+            break;
+        }
+    }
+
+    // At this point it is known its string representation of float value.
+    // Its full length still remains to be determined.
+
+    if (currentOffset == size)
+    {
+        length = currentOffset - startOffset;
+        return true;
+    }
+
+    // Verify optional e/E notation
+    if (buffer[currentOffset] == 'e' ||
+        buffer[currentOffset] == 'E')
+    {
+        ++currentOffset;
+
+        if (currentOffset == size)
+        {
+            // 'E' should be followed by cypher
+            return false;
+        }
+
+        // Verify optional negative sign
+        bool sign2 = false;
+        if (buffer[currentOffset] == '-')
+        {
+            sign2 = true;
+            ++currentOffset;
+
+            if (currentOffset == size)
+            {
+                // 'E-' should be followed by cypher
+                return false;
+            }
+        }
+
+        // Iterate over cyphers of E notation
+        bool Ecyphers = false;
+        for(; currentOffset<size; ++currentOffset)
+        {
+            if (isCypher(buffer[currentOffset]))
+            {
+                Ecyphers = true;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        if (!Ecyphers)
+        {
+            // E[-] should be followed by cypher
+            return false;
+        }
+
+        if (currentOffset == size)
+        {
+            length = currentOffset - startOffset;
+            return true;
+        }
+    }
+
+    if (buffer[currentOffset] == 'f' ||
+        buffer[currentOffset] == 'F')
+    {
+        ++currentOffset;
+    }
+
+    length = currentOffset - startOffset;
+    return true;
+}
+
+bool Parser::isInteger(const uint64 startOffset, uint64& length)
+{
+    // Integer notation:
+    // 
+    // [-/+][cccc]
+    //
+    // - negative sign is optional
+    // - integer part is required
+    //
+    // Minimum case: c
+    if (startOffset >= size)
+    {
+        return false;
+    }
+
+    uint64 currentOffset = startOffset;
+
+    // Verify optional sign
+    if (buffer[currentOffset] == '-' ||
+        buffer[currentOffset] == '+')
+    {
+        ++currentOffset;
+    }
+
+    // '-' string is not a number
+    if (currentOffset == size)
+    {
+        return false;
+    }
+
+    // Verify integer part
+    bool integerPart = false;
+    for(; currentOffset<size; ++currentOffset)
+    {
+        if (isCypher(buffer[currentOffset]))
+        {
+            integerPart = true;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    if (integerPart)
+    {
+        length = currentOffset - startOffset;
+        return true;
+    }
+
+    return false;
+}
+
+bool Parser::isString(const uint64 startOffset, sint32& length)
+{
+    // Minimum case: c
+    if (startOffset >= size)
+    {
+        return false;
+    }
+
+    // String needs to start at startOffset 
+    // (there can be no whitespaces, EOL, EOF, etc.)
+    if (!isCharacter(buffer[startOffset]))
+    {
+        return false;
+    }
+
+    uint64 currentOffset = startOffset;
+
+    for(; currentOffset<size; ++currentOffset)
+    {
+        if (!isCharacter(buffer[currentOffset]))
+        {
+            break;
+        }
+    }
+
+    length = sint32(currentOffset - startOffset);
+    return true;
+}
+
+bool Parser::readU64(uint64& value)
+{
+    // Last found type is not a integer number
+    if (foundNumberLength == 0 ||
+        type != ParserType::Integer)
+    {
+        return false;
+    }
+
+    // Cannot read negative value to unsigned integer
+    if (buffer[foundNumberOffset] == '-')
+    {
+        return false;
+    }
+
+    char* ending = nullptr;
+    value = strtoul((const char*)&buffer[foundNumberOffset], &ending, 10);
+    return true;
+}
+
+bool Parser::readS64(sint64& value)
+{
+    // Last found type is not a integer number
+    if (foundNumberLength == 0 ||
+        type != ParserType::Integer)
+    {
+        return false;
+    }
+
+    char* ending = nullptr;
+    value = strtol((const char*)&buffer[foundNumberOffset], &ending, 10);
+    return true;
+}
+
+bool Parser::readF32(float& value)
+{
+    // Last found type is not floating point number
+    if (foundNumberLength == 0 ||
+        type != ParserType::Float)
+    {
+        return false;
+    }
+
+    char* ending = nullptr;
+    value = strtof((const char*)&buffer[foundNumberOffset], &ending);
+    return true;
+}
+
+bool Parser::readF64(double& value)
+{
+    // Last found type is not floating point number
+    if (foundNumberLength == 0 ||
+        type != ParserType::Float)
+    {
+        return false;
+    }
+
+    char* ending = nullptr;
+    value = strtod((const char*)&buffer[foundNumberOffset], &ending);
+    return true;
+}
+
+const char* Parser::string(void)
+{
+    if (foundStringOffset < 0)
+    {
+        return nullptr;
+    }
+
+    return (const char*)&buffer[foundStringOffset];
+}
+
+uint32 Parser::stringLength(void)
+{
+    if (type != ParserType::String)
+    {
+        return 0;
+    }
+
+    return foundStringLength;
+}
+
+
+bool Parser::isStringMatching(const char* string)
+{
+    // Last found type is not string
+    if (foundStringLength <= 0 ||
+        type != ParserType::String)
+    {
+        return false;
+    }
+
+    // Verifies that both strings have the same length
+    const size_t inLength = strlen(string);
+    if (foundStringLength != inLength)
+    {
+        return false;
+    }
+
+    if (strncmp((const char *)&buffer[foundStringOffset], string, foundStringLength) == 0)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+
+
 
 bool Parser::read(std::string& word, bool& eol)
 {
@@ -171,24 +615,43 @@ bool Parser::end(void)
     return (offset >= size);
 }
 
-bool isCypher(uint8 input)
+bool isCypher(const uint8 input)
 {
     return ((input > 47) && (input < 58));
 }
 
-bool isCharacter(uint8 input)
+bool isHexCypher(const uint8 input)
+{
+    return (((input > 47) && (input < 58)) ||  // [0,9]
+            ((input > 64) && (input < 71)) ||  // [A,F]
+            ((input > 96) && (input < 103)));  // [a,f]
+}
+
+bool isUpperCaseLetter(const uint8 input)
+{
+    return ((input > 64) && (input < 91));
+}
+
+bool isLowerCaseLetter(const uint8 input)
+{
+    return ((input > 96) && (input < 123));
+}
+
+bool isLetter(const uint8 input)
+{
+    return ((input > 64) && (input < 91)) ||
+           ((input > 96) && (input < 123));
+}
+
+bool isCharacter(const uint8 input)
 {
     return ((input > 32) && (input < 127));
 }
 
-bool isWhitespace(uint8 input)
+bool isWhitespace(const uint8 input)
 {
     if ( input == ' '  || 
-         input == '\t' || 
-         input == '\r' || 
-         input == '\n' || 
-         input == '\v' || 
-         input == '\f' ) 
+         input == '\t' ) 
     {
         return true;
     }
@@ -196,7 +659,7 @@ bool isWhitespace(uint8 input)
     return false;
 }
 
-bool isEol(uint8 input)
+bool isEol(const uint8 input)
 {
     if ( input == '\r' || 
          input == '\n' || 
@@ -371,6 +834,169 @@ bool isFloat(const char* text, const uint32 length)
     }
 
     return false;
+}
+
+ParsingResult parseString(const uint8* buffer, const uint64 size, uint64& length)
+{
+    if (!buffer || size == 0)
+    {
+        return ParsingResult::IncompleteData;
+    }
+
+    uint64 offset = 0;
+
+    // Check for empty string
+    if (buffer[offset] == 0)
+    {
+        length = 1;
+        ParsingResult::Success;
+    }
+
+    // String needs to start at startOffset 
+    // (there can be no whitespaces, EOL, EOF, etc.)
+    if (!isCharacter(buffer[offset]))
+    {
+        return ParsingResult::InvalidFormat;
+    }
+
+    for (; offset < size; ++offset)
+    {
+        if (!isCharacter(buffer[offset]))
+        {
+            if (buffer[offset] != 0)
+            {
+                // String should be null terminated
+                return ParsingResult::InvalidFormat;
+            }
+
+            // Detected end of null terminated string.
+            // Returns string length including terminating zero.
+            length = offset + 1;
+            return ParsingResult::Success;
+        }
+    }
+
+    return ParsingResult::IncompleteData;
+}
+
+// Returns success if buffer content is byte sequence ending with EOL (LF or CRLF).
+// In such case returned length is length of line without EOL bytes, while offset
+// points to what would be next byte after EOL signature (so line length plus size 
+// of EOL bytes).
+ParsingResult isLine(const uint8* buffer, const uint64 size, uint64& length, uint64& offset)
+{
+    if (!buffer || size == 0)
+    {
+        return ParsingResult::IncompleteData;
+    }
+
+    // Finds length of the line
+    uint32 i = 0;
+    while(i < size && !isEol(buffer[i]))
+    {
+        i++;
+    }
+
+    // EOL terminator was not found
+    if (i == size)
+    {
+        return ParsingResult::InvalidFormat;
+    }
+
+    length = i;
+
+    // CRLF case
+    if (buffer[i] == '\r') // [[ unlikely ]]
+    {
+        i++;
+        if (i >= size) // [[ unlikely ]]
+        {
+            return ParsingResult::IncompleteData;
+        }
+
+        if (buffer[i] != '\n')
+        {
+            return ParsingResult::InvalidFormat;
+        }
+    }
+    i++;
+    
+    offset = i;
+    return ParsingResult::Success;
+}
+
+// Returns success if buffer content is string followed by EOL (LF or CRLF) 
+// and its matching input string (which is null terminated but that null is 
+// not taken into notice during comparison). In such case returned length is 
+// offset to what would be next byte after EOL signature (so string length 
+// plus size of EOL bytes).
+ParsingResult isLineMatching(const uint8* buffer, const uint64 size, const char* string, uint64& length)
+{
+    if (!buffer || size == 0 || !string || strlen(string) > size)
+    {
+        return ParsingResult::IncompleteData;
+    }
+
+    // Compares buffer content to input string (ignoring terminating bytes for both)
+    if (strncmp((const char*)buffer, string, strlen(string)) != 0)
+    {
+        return ParsingResult::InvalidFormat;
+    }
+
+    uint64 offset = strlen(string);
+    if (offset == size)
+    {
+        ParsingResult::IncompleteData;
+    }
+
+    // String is followed by EOL (LF or CRLF)
+    if (!isEol(buffer[offset]))
+    {
+        return ParsingResult::InvalidFormat;
+    }
+    // CRLF case
+    if (buffer[offset] == '\r') // [[ unlikely ]]
+    {
+        offset++;
+        if (offset >= size) // [[ unlikely ]]
+        {
+            return ParsingResult::IncompleteData;
+        }
+
+        if (buffer[offset] != '\n')
+        {
+            return ParsingResult::InvalidFormat;
+        }
+    }
+    offset++;
+
+    length = offset;
+    return ParsingResult::Success;
+}
+
+bool nextWord(const std::string_view& line, uint64& lineOffset, std::string_view& word)
+{
+    if (lineOffset > line.size())
+    {
+        return false;
+    }
+
+    size_t spaceOffset = line.find(' ', lineOffset);
+    if (spaceOffset == std::string::npos)
+    {
+        // See if its not last word in line
+        word = line.substr(lineOffset);
+        if (!word.size())
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    word = line.substr(lineOffset, spaceOffset - lineOffset);
+    lineOffset = spaceOffset + 1;
+    return true;
 }
 
 } // en
